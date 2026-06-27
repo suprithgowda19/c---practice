f75f47ed3925c4336735cc77b7194625ee13b291
Suprith Gowda <suprith.19gowda@gmail.com>
2026-06-27 07:25:12 +0530

    intersection  of two sorted arrays

--- demo.cpp
+++ demo.cpp
@@ -1,54 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void shift_all_zeros_right(vector<int>&arr)
+void intersection(vector<int>&a,vector<int>&b)
 {
-  int n=arr.size();
-   int i=0;
-   int j=-1;
-  for( i=0;i<n;i++)
-  {
-    if(arr[i]==0)
-    {
-      j=i;
-      break;
-    }
-    
-  }
-  if(i==-1)
-  {
-    return ;
-  }
-  i=j+1;
-  
-  while(i<n)
-  {  
-    
-    if(arr[i] != 0)
-    {
-      swap(arr[i],arr[j]);
-      j++;
-    }
-    else{
-      i++;
-    }
- }
- for(i=0;i<n;i++){
-  cout<<arr[i];
- }
+    int i=0,j=0;
+    int n1=a.size();
+    int n2=b.size();
+    vector<int>intersection;
+
+   
+      while(i<n1 && j<n2)
+      {
+           if(a[i]==b[j])
+           {
+              intersection.push_back(a[i]);
+              i++;
+              j++;
+           }
+           else if(a[i]<b[j])
+           {
+             i++;
+           }
+
+           else
+           j++;
+      }
+      for(i=0;i<intersection.size();i++)
+      {
+        cout<<intersection[i];
+      }
+   
+
      
 }
 
 
 int main()
 {   
-  int n=5;
-    vector<int>arr(n);
-  for(int i = 0; i < n; i++)
+  int n1=5,n2=6;
+    vector<int>a(n1);
+    vector<int>b(n2);
+  for(int i = 0; i < n1; i++)
   {
-    cin >> arr[i];
+    cin >> a[i];
   }
-    shift_all_zeros_right(arr);
+  for(int i=0;i<n2;i++)
+  {
+    cin>>b[i];
+  }
+  intersection(a,b);
+  
 
 
   return 0;
